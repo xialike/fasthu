6992cb12e22d5daf40e45d19ea16b05570b3d23a
xialike <116798358+xialike@users.noreply.github.com>
2023-11-30 11:37:26 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='9be404b4-3104-43ea-8632-d81d10d1780f'
-VMESS_WSPATH='/9be404b4-3104-43ea-8632-d81d10d1780f-vmess'
-VLESS_WSPATH='/9be404b4-3104-43ea-8632-d81d10d1780f-vless'
+UUID='1646b69a-0a3e-4b57-9139-5cf52db32b16'
+VMESS_WSPATH='/1646b69a-0a3e-4b57-9139-5cf52db32b16-vmess'
+VLESS_WSPATH='/1646b69a-0a3e-4b57-9139-5cf52db32b16-vless'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER=''
